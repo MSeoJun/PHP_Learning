02f36ecc7deb8146c602848ed1c794515fd16ef3
MSeoJun <mun3582w@naver.com>
2020-12-15 16:38:58 +0900

    PHP와 숫자

---
+++ number.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h3>1 + 1</h3>
+<?php
+echo 1 + 1
+?>
+
+<h3>3 - 2</h3>
+<?php
+echo 3 - 2
+?>
+
+<h3>4 * 2</h3>
+<?php
+echo 4 * 2
+?>
+
+<h3>4 / 2</h3>
+<?php
+echo 4 / 2
+?>
+</body>
+</html>
