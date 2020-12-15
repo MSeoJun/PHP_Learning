046e273cd80ee90d5a6c0822d7d53abf6423b5a1
MSeoJun <mun3582w@naver.com>
2020-12-15 16:49:28 +0900

    strlen함수

---
+++ function.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>function</h1>
+    <?php
+    $str = "Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Nulla totam quidem nihil maxime,
+    nisi repudiandae earum facere rem,
+    voluptatibus laborum iusto possimus ut deserunt,
+    iste aliquam? Nostrum dolorum esse nihil.";
+    echo $str;
+    ?>
+    <h2>strlen()</h2>
+    <?php
+    echo "문자열 길이 : ";
+    echo strlen($str);
+    ?>
+</body>
+</html>
