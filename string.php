727406fffd5751ea3570ec818d855b9567777ee1
MSeoJun <mun3582w@naver.com>
2020-12-15 16:39:10 +0900

    PHP와 문자열

---
+++ string.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+//문자열을 합칠때
+echo "Hello "."world";
+?>
+
+<?php
+// 문자열 길이
+echo strlen("seojun2")
+?>
+</body>
+</html>
