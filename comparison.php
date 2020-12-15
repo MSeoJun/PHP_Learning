9a97eff507d387803acb446bc48f9fd4086ed15d
MSeoJun <mun3582w@naver.com>
2020-12-15 16:43:10 +0900

    PHP의 비교연산자

---
+++ comparison.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    var_dump(1 == 1); //true
+    var_dump(1 > 1); // false
+    var_dump(1 < 4); // true
+    ?>
+</body>
+</html>
