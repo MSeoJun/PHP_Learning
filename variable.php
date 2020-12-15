9e9def488dda465b3ddc5962c03e111dbc5a438a
MSeoJun <mun3582w@naver.com>
2020-12-15 16:39:32 +0900

    PHP의 변수

---
+++ variable.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $name = "seojun";
+    echo "Lorem ipsum dolor sit amet,".$name." consectetur adipisicing elit. Cumque, molestias perferendis. Ad, ex, 
+    seojunaperiam aut voluptas eius perspiciatis fugiat ea illo quos impedit recusandae, 
+    magni veritatis perferendis voluptates quod".$name."repudiandae!"
+    ?>
+</body>
+</html>
